5b0dddcafb26b51545b891e7e74af0df14186818
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-18 14:48:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-    if board[0] == "" || board[0] == " "
+    if board[0] == "" || board[0] == " " || board[0] == nil
         return false
     end
 end
